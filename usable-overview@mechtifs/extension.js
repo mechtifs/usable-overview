6f02a657465d2f40f301e811db847bafb481e7ba
mechtifs <mechtifs@gmail.com>
2022-03-12 10:38:21 +0800

    Update

---
+++ usable-overview@mechtifs/extension.js
@@ -0,0 +1,23 @@
+const Main = imports.ui.main;
+
+let _indicator;
+
+function _showApps(widget, event) {
+	if (event.get_button() == 3) {
+		Main.overview._overview._controls._toggleAppsPage()
+	}	
+}
+
+function init() {
+}
+
+function enable() {
+    _indicator = Main.panel.statusArea['activities'].connect('button-release-event', _showApps);
+	Main.overview.dash.showAppsButton.hide();
+}
+
+function disable() {
+	Main.panel.statusArea['activities'].disconnect(_indicator);
+	Main.overview.dash.showAppsButton.show();
+}
+
